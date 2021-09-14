2c32e29702d6d52ebc177f007c66eff9c1060beb
Paulo Oliveira <84526032+PJ-Oliveira@users.noreply.github.com>
2021-09-14 17:53:35 -0300

    Correções realizadas

--- src/main/java/com/pj/offer/advice/handler/OfferControllerAdvice.java
+++ src/main/java/com/pj/offer/advice/handler/OfferControllerAdvice.java
@@ -1,19 +1,17 @@
 package com.pj.offer.advice.handler;
 
 import javax.servlet.http.HttpServletRequest;
-
 import com.pj.offer.advice.exception.NotFoundException;
 import com.pj.offer.advice.exception.ErrorAtributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
 
 @ControllerAdvice
-public class OfferControllerAdvice extends ResponseEntityExceptionHandler {
+public class OfferControllerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorAtributes> notFoundException(NotFoundException notFoundException, HttpServletRequest request){
