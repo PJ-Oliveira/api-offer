127bda9455c6b27055ba598b0ea4218d283097c4
Paulo Oliveira <84526032+PJ-Oliveira@users.noreply.github.com>
2021-09-15 17:40:58 -0300

    Teste para disableOffer
    Acrescentado teste para o método disableOfferByIdProduct

--- src/test/java/com/pj/offer/service/OfferServiceTest.java
+++ src/test/java/com/pj/offer/service/OfferServiceTest.java
@@ -6,25 +6,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 import com.pj.offer.advice.exception.NotFoundException;
 import com.pj.offer.config.modelmapper.ModelMapperConfig;
 import com.pj.offer.domain.dto.DeleteOfferDto;
 import com.pj.offer.domain.Offer;
 import com.pj.offer.domain.form.OfferForm;
 import com.pj.offer.repository.OfferRepository;
+
 import java.util.ArrayList;
 import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
 @ContextConfiguration(classes = {OfferService.class, ModelMapper.class, ModelMapperConfig.class})
@@ -44,7 +51,8 @@ class OfferServiceTest {
         when(this.offerRepository.findAll((org.springframework.data.domain.Pageable) any()))
                 .thenReturn(new PageImpl<>(new ArrayList<>()));
         assertTrue(this.offerService.findAll(null).isEmpty());
-        verify(this.offerRepository, times(1)).findAll((org.springframework.data.domain.Pageable) any());}
+        verify(this.offerRepository, times(1)).findAll((org.springframework.data.domain.Pageable) any());
+    }
 
 
     @Test
@@ -53,7 +61,8 @@ class OfferServiceTest {
         var page = TestTemplet.newPage();
         when(offerRepository.findAll(pageable)).thenReturn(page);
         offerService.findAll(pageable);
-        verify(this.offerRepository, times(1)).findAll(pageable);}
+        verify(this.offerRepository, times(1)).findAll(pageable);
+    }
 
     @Test
     public void deleteOffer_WhenDeleteOfferById_ExpectedSuccess() {
@@ -62,7 +71,8 @@ class OfferServiceTest {
         when(this.offerRepository.getById((Long) any())).thenReturn(offer);
         this.offerService.deleteOffer(1L);
         verify(this.offerRepository).deleteById((Long) any());
-        verify(this.offerRepository).getById((Long) any());}
+        verify(this.offerRepository).getById((Long) any());
+    }
 
     @Test
     public void getOfferByValidId_WhenFindOfferUnexpired_ExpectedSuccess() {
@@ -70,23 +80,25 @@ class OfferServiceTest {
         Optional<Offer> ofResult = Optional.<Offer>of(offer);
         when(this.offerRepository.getOnlyUnexpiredOfferById((Long) any())).thenReturn(ofResult);
         this.offerService.getOfferByValidId((Long) any());
-        verify(this.offerRepository).getOnlyUnexpiredOfferById((Long) any());}
+        verify(this.offerRepository).getOnlyUnexpiredOfferById((Long) any());
+    }
 
     @Test
     public void getOfferByValidId_WhenFindOfferUnexpiredAndThrowingException_ExpectedSuccess() {
         Offer offer = new Offer();
         when(offerRepository.getOnlyUnexpiredOfferById(eq(offer.getId()))).thenReturn(Optional.empty());
-        assertThatThrownBy(()-> offerService.getOfferByValidId(any()))
+        assertThatThrownBy(() -> offerService.getOfferByValidId(any()))
                 .isInstanceOf(NotFoundException.class)
                 .hasMessageContaining("Id " + offer.getId() + " Not Found");
-        verify(this.offerRepository, times(1)).getOnlyUnexpiredOfferById(any());}
-
+        verify(this.offerRepository, times(1)).getOnlyUnexpiredOfferById(any());
+    }
 
     @Test
-    void deleteOfferByIdProduct_WhenDeleteOfferByIdProduct_ExpectedSuccess() {
-        doNothing().when(this.offerRepository).deleteOfferByProduct((Long) any());
-        this.offerService.deleteOfferByIdProduct(new DeleteOfferDto());
-        verify(this.offerRepository, times(1)).deleteOfferByProduct((Long) any());}
+    void testDisableOffer_WhenDisableOfferByIdProduct_ExpectedSuccess() {
+        doNothing().when(this.offerRepository).disableOfferByIdProduct((Long) any());
+        this.offerService.disableOffer(new DeleteOfferDto());
+        verify(this.offerRepository).disableOfferByIdProduct((Long) any());
+    }
 
     @Test
     void save_WhenSaveOffer_ExpectedSuccess() {
@@ -94,6 +106,7 @@ class OfferServiceTest {
         OfferForm offerForm = new OfferForm();
         when(this.offerRepository.save((any()))).thenReturn(offer);
         this.offerService.save(offerForm);
-        verify(this.offerRepository, times(1)).save(any());}
+        verify(this.offerRepository, times(1)).save(any());
+    }
 
 }
